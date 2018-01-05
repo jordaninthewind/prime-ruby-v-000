a34779b7ee5f898672aad2a6c54a6cdfd2e799ab
Jordan Kline <jordan.kline@gmail.com>
2018-01-05 21:54:23 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,18 +1,11 @@
-require 'pry'
-
 def prime?(num)
-p num
 factors = []
   if num < 3
     return false
   else
-    # binding.pry
     (1..num).to_a.each do | el |
       factors << el if num % el == 0
     end
   end
-
-  p factors
-
   factors.length > 2 ? false : true
 end
