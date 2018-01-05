a5e5ee75475f3b7554f50c4baeb9d9317cf6d800
Jordan Kline <jordan.kline@gmail.com>
2018-01-05 21:30:47 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,17 @@
 def prime?(num)
+factors = []
+
   if num < 2
     return false
   else
-    factors = []
     (3...num).to_a.each do | el |
       if num % el == 0
         factors << el
       end
     end
-  if factors > 1
+  if factors.length > 1
     false
   else
     true
-  end          
+  end
 end
