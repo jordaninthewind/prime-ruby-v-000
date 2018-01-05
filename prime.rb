59b5c81c44b2260d6016313537083f71310840fd
Jordan Kline <jordan.kline@gmail.com>
2018-01-05 21:30:01 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,16 @@
 def prime?(num)
-
+  if num < 2
+    return false
+  else
+    factors = []
+    (3...num).to_a.each do | el |
+      if num % el == 0
+        factors << el
+      end
+    end
+  if factors > 1
+    false
+  else
+    true
+  end          
 end
